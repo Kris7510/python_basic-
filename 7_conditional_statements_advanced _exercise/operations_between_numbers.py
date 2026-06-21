fd714d38700783f1bbb937116c720c576fd8b811
Kris7510 <ralchev.kristian@gmail.com>
2026-06-21 21:47:43 +0300

    calculator

---
+++ 7_conditional_statements_advanced _exercise/operations_between_numbers.py
@@ -0,0 +1,50 @@
+n1 = int(input())
+n2 = int(input())
+operation = input()
+result  = 0
+
+if operation == "+":
+    result = n1 + n2
+    if result % 2 == 0:
+        print(f"{n1} + {n2} = {result} - even")
+    elif result % 2 != 0:
+        print(f"{n1} + {n2} = {result} - odd")
+
+elif operation == "-":
+    result = n1 - n2
+    if result % 2 == 0:
+       print(f"{n1} - {n2} = {result} - even")
+    elif result % 2 != 0:
+       print(f"{n1} - {n2} = {result} - odd")
+
+elif operation == "*":
+    result = n1 * n2
+    if result % 2 == 0:
+        print(f"{n1} * {n2} = {result} - even")
+    elif result % 2 != 0:
+        print(f"{n1} * {n2} = {result} - odd")
+
+elif operation == "/":
+
+    if n1 == 0 or n2 == 0:
+        print(f"Cannot divide {n1} by zero")
+    else:
+        result = n1 / n2
+        print(f"{n1} / {n2} = {result:.2f}")
+
+elif operation == "%":
+
+    if n1 == 0 or n2 == 0:
+        print(f"Cannot divide {n1} by zero")
+    else:
+        result = n1 % n2
+        print(f"{n1} % {n2} = {result}")
+
+
+
+
+
+
+
+
+
